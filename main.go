3c5e67072796c7ff32a8f95ee8d40520bc85db8f
fadhilthomas <mthomasfadhilayahya@gmail.com>
2021-10-28 13:39:44 +0700

    feat: enumerate page result

--- main.go
+++ main.go
@@ -10,8 +10,8 @@ import (
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/rs/zerolog/pkgerrors"
-	"golang.org/x/oauth2"
 	"go.uber.org/ratelimit"
+	"golang.org/x/oauth2"
 )
 
 var (
@@ -40,99 +40,109 @@ func main() {
 	tc := oauth2.NewClient(ctx, ts)
 	client := github.NewClient(tc)
 
-	repositoryListByOrgOptions := &github.RepositoryListByOrgOptions{
-		ListOptions: github.ListOptions{PerPage: 10},
-	}
+	repositoryListByOrgOptions := &github.RepositoryListByOrgOptions{}
 
-	listCollaboratorsOptions := &github.ListCollaboratorsOptions{}
+	collaboratorsListOptions := &github.ListCollaboratorsOptions{}
 
 	// get all pages of results
 	for {
-		repositoryList, resp, err := client.Repositories.ListByOrg(ctx, organizationName, repositoryListByOrgOptions)
+		repositoryList, repositoryResp, err := client.Repositories.ListByOrg(ctx, organizationName, repositoryListByOrgOptions)
 		if err != nil {
 			log.Error().Stack().Err(errors.New(err.Error())).Msg("")
 			continue
 		}
-		if resp.StatusCode != 200 {
+		if repositoryResp.StatusCode != 200 {
 			continue
 		}
 
 		for _, repository := range repositoryList {
 			repositoryName := *repository.Name
 			repositoryOwner := *repository.Owner.Login
-			userList, _, err := client.Repositories.ListCollaborators(ctx, repositoryOwner, repositoryName, listCollaboratorsOptions)
 
-			if err != nil {
-				log.Error().Stack().Err(errors.New(err.Error())).Msg("")
-				continue
-			}
+			// get all collaborator
+			for {
+				collaboratorsList, collaboratorsResp, err := client.Repositories.ListCollaborators(ctx, repositoryOwner, repositoryName, collaboratorsListOptions)
 
-			// get all page with repository name
-			rl.Take()
-			githubRepositoryNotionList, err := model.QueryNotionRepository(notionDatabase, repositoryName)
-			if err != nil {
-				log.Error().Stack().Err(err).Msg("")
-				return
-			}
-
-			// if list of repository name page not empty
-			// update all status to close
-			if len(githubRepositoryNotionList) > 0 {
-				for _, githubRepositoryNotionPage := range githubRepositoryNotionList {
-					rl.Take()
-					_, err = model.UpdateNotionRepositoryStatus(notionDatabase, githubRepositoryNotionPage.ID.String(), "close")
-					if err != nil {
-						log.Error().Stack().Err(err).Msg("")
-						return
-					}
+				if err != nil {
+					log.Error().Stack().Err(errors.New(err.Error())).Msg("")
+					continue
 				}
-			}
 
-			for _, user := range userList {
-				githubRepository := model.GitHubRepository{}
-				githubRepository.OrganizationName = organizationName
-				githubRepository.RepositoryName = repositoryName
-				githubRepository.RepositoryOwner = repositoryOwner
-				githubRepository.UserLogin = *user.Login
-				githubRepository.Permission = user.Permissions
+				if collaboratorsResp.StatusCode != 200 {
+					continue
+				}
 
+				// get all page with repository name
 				rl.Take()
-				// get page with repository name and user
-				githubRepositoryUserNotion, err := model.QueryNotionRepositoryUser(notionDatabase, repositoryName, *user.Login)
+				githubRepositoryNotionList, err := model.QueryNotionRepository(notionDatabase, repositoryName)
 				if err != nil {
 					log.Error().Stack().Err(err).Msg("")
 					return
 				}
 
-				// if list of repository name and user page empty
-				// insert to notion
-				if len(githubRepositoryUserNotion) == 0 {
-					rl.Take()
-					_, err = model.InsertNotionRepository(notionDatabase, "report-log", githubRepository)
-					if err != nil {
-						log.Error().Stack().Err(err).Msg("")
-						continue
+				// if list of repository name page not empty
+				// update all status to close
+				if len(githubRepositoryNotionList) > 0 {
+					for _, githubRepositoryNotionPage := range githubRepositoryNotionList {
+						rl.Take()
+						_, err = model.UpdateNotionRepositoryStatus(notionDatabase, githubRepositoryNotionPage.ID.String(), "close")
+						if err != nil {
+							log.Error().Stack().Err(err).Msg("")
+							return
+						}
 					}
+				}
+
+				for _, user := range collaboratorsList {
+					githubRepository := model.GitHubRepository{}
+					githubRepository.OrganizationName = organizationName
+					githubRepository.RepositoryName = repositoryName
+					githubRepository.RepositoryOwner = repositoryOwner
+					githubRepository.UserLogin = *user.Login
+					githubRepository.Permission = user.Permissions
 
 					rl.Take()
-					_, err = model.InsertNotionRepository(notionDatabase, "change-log", githubRepository)
-					if err != nil {
-						log.Error().Stack().Err(err).Msg("")
-						continue
-					}
-				} else {
-					rl.Take()
-					_, err = model.UpdateNotionRepository(notionDatabase, githubRepositoryUserNotion[0].ID.String(), githubRepository, "open")
+					// get page with repository name and user
+					githubRepositoryUserNotion, err := model.QueryNotionRepositoryUser(notionDatabase, repositoryName, *user.Login)
 					if err != nil {
 						log.Error().Stack().Err(err).Msg("")
 						return
 					}
+
+					// if list of repository name and user page empty
+					// insert to notion
+					if len(githubRepositoryUserNotion) == 0 {
+						rl.Take()
+						_, err = model.InsertNotionRepository(notionDatabase, "report-log", githubRepository)
+						if err != nil {
+							log.Error().Stack().Err(err).Msg("")
+							continue
+						}
+
+						rl.Take()
+						_, err = model.InsertNotionRepository(notionDatabase, "change-log", githubRepository)
+						if err != nil {
+							log.Error().Stack().Err(err).Msg("")
+							continue
+						}
+					} else {
+						rl.Take()
+						_, err = model.UpdateNotionRepository(notionDatabase, githubRepositoryUserNotion[0].ID.String(), githubRepository, "open")
+						if err != nil {
+							log.Error().Stack().Err(err).Msg("")
+							return
+						}
+					}
 				}
+				if collaboratorsResp.NextPage == 0 {
+					break
+				}
+				collaboratorsListOptions.Page = collaboratorsResp.NextPage
 			}
 		}
-		if resp.NextPage == 0 {
+		if repositoryResp.NextPage == 0 {
 			break
 		}
-		repositoryListByOrgOptions.Page = resp.NextPage
+		repositoryListByOrgOptions.Page = repositoryResp.NextPage
 	}
 }
